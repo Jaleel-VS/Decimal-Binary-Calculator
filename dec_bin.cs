3c54e9571cb2533fc1b034e478cb2718beadf18e
Jaleel <56233547+Jaleel-VS@users.noreply.github.com>
2020-07-26 10:35:00 +0200

    Update dec_bin.cs

--- dec_bin.cs
+++ dec_bin.cs
@@ -3,14 +3,15 @@ using System;
 class Part2
 {
 
-    public static int binToDec(string bin_string) {
+    public static int binToDec(string bin_string)
+    {
 
         int length = bin_string.Length;
         double dec_value = 0;
         int count = 0;
         double exponent = 0;
         double num_val = 0;
-        for (int i = length-1; i >= 0; i--)
+        for (int i = length - 1; i >= 0; i--)
         {
             exponent = Math.Pow(2, i);
             num_val = Char.GetNumericValue(bin_string[count]);
@@ -18,13 +19,12 @@ class Part2
             count++;
         }
 
-        return (int)dec_value;   
+        return (int)dec_value;
     }
-    public static string decToBin(int number) {
-        
-        string bin = "";
+    public static string decToBin(int number)
+    {
 
-        Console.Write($"\n{number} "); //the start of the console statement
+        string bin = "";
         while (number > 0)
         {
             if (number % 2 == 0)
@@ -48,28 +48,30 @@ class Part2
         string _binary;
 
         Console.WriteLine("Pick an option, please: \n1 - Decimal to binary\n2 - Binary to Decimal\nPress any other character to quit");
-        char choice = Char.Parse(Console.ReadLine());
+        
+        
+        string choice = Console.ReadLine();
 
 
-        while (choice == '1' || choice == '2')
+        while (choice == "1" || choice == "2")
         {
-            if (choice == '1')
+            if (choice == "1")
             {
                 Console.WriteLine("Enter an interger to convert to binary: ");
                 _decimal = Int32.Parse(Console.ReadLine());
-                Console.Write($"{_decimal} converted to binary is {decToBin(_decimal)}");
+                Console.WriteLine($"converted to binary is {decToBin(_decimal)}");
             }
-            if (choice == '2')
+            if (choice == "2")
             {
                 Console.WriteLine("Enter a binary number to convert to an interger: ");
                 _binary = Console.ReadLine();
-                Console.Write($"{_binary} converted to binary is {binToDec(_binary)}");
+                Console.WriteLine($"converted to binary is {binToDec(_binary)}");
             }
 
 
             Console.WriteLine("\n\n_______________________\n\nWelcome back! :D\nPick an option, please: \n1 - Decimal to binary\n2 - Binary to Decimal\nPress any other character to quit");
-            choice = Char.Parse(Console.ReadLine());
-        } 
+            choice = Console.ReadLine();
+        }
 
         Console.WriteLine("Goodbye!");
     }
